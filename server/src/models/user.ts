72e5ac6bc4e7d03a82ea1f1b3c5a87bd93b1f469
matheovieilleville <matheo20055@gmail.com>
2025-11-24 11:36:46 +0100

    add story controllers and routes for CRUD operations

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -4,7 +4,7 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  role?: 'user' | 'admin' | 'author';
+  role?: 'user' | 'admin';
   createdAt: Date;
 }
 
@@ -12,7 +12,7 @@ const UserSchema: Schema<IUser> = new Schema<IUser>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-    role: {type: String, enum: ['user', 'admin', 'author'], required: true},
+    role: {type: String, enum: ['user', 'admin'], required: true},
   createdAt: { type: Date, default: Date.now }
 });
 
