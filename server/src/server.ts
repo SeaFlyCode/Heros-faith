2c9b0ecf85e422423c4bcb2e8771110646b3b153
matheovieilleville <matheo20055@gmail.com>
2025-11-24 12:07:30 +0100

    update environment configuration and server initialization with database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import { errorHandler } from './middlewares/errorHandler.js';
+import { errorHandler } from './middlewares/errorHandler.ts';
+import { connectDB } from './config/database.ts';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +14,20 @@ app.get('/', (req, res) => {
 });
 
 app.use(errorHandler);
+
+async function startServer() {
+    try {
+        // Initialisation de la connexion à la base de données ou autres services
+        console.log('Initialisation des services...');
+        await connectDB();
+    } catch (error) {
+        console.error('Erreur lors de l\'initialisation des services :', error);
+        process.exit(1);
+    }
+
+}
+
+await startServer();
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
